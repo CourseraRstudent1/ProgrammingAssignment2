b809c8bfe4e5b8c957b87b2363b87ea05dca5f65
CourseraRstudent1 <se266@cam.ac.uk>
2014-07-24 15:48:28 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,10 @@
+# The following two functions, when used together, calculate and cache the inverse of
+# an invertible square matrix.
+
+
 # The makeCacheMatrix() function:
 # -------------------------------
-# 1) Generates a matrix and a cache for storing the inverse of that matrix.
+# 1) Takes a matrix and generates a cache for storing the inverse of that matrix.
 # 2) The matrix passed to the function must be an invertible square matrix.
 
 # e.g. matrix1 <- makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
@@ -9,17 +13,17 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
-        set <- function(y) {                                                    # the set() function is used to modify the matrix e.g. matrix1$set(matrix(c(2,3,4,5), nrow=2, ncol=2)).
+        set <- function(y) {                                    # the set() function is used to modify the matrix e.g. matrix1$set(matrix(c(2,3,4,5), nrow=2, ncol=2)).
                 x <<- y
                 i <<- NULL
         }
-        get <- function() {x}                                                   # the get() function returns the original matrix e.g. matrix1$get().
-        setinverse <- function(inverse) {i <<- inverse}                         # the setinverse() function is used by the cacheSolve() function to cache the inverse of the matrix.
-        getinverse <- function() {i}                                            # the getinverse() function returns the cached inverse of the matrix (if it has been cached) e.g. matrix1$getinverse().
+        get <- function() {x}                                   # the get() function returns the original matrix e.g. matrix1$get().
+        setinverse <- function(inverse) {i <<- inverse}         # the setinverse() function is used by the cacheSolve() function to cache the inverse of the matrix.
+        getinverse <- function() {i}                            # the getinverse() function returns the cached inverse of the matrix (if it has already been cached) e.g. matrix1$getinverse().
         list(set = set,... =
              get = get,
              setinverse = setinverse,
-             getinverse = getinverse)                                           # the makeCacheMatrix() function is a list of 4 functions: set(), get(), setinverse(), getinverse().
+             getinverse = getinverse)                           # the makeCacheMatrix() function is a list of 4 functions: set(), get(), setinverse(), getinverse().
 }
 
 
@@ -32,14 +36,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 cacheSolve <- function(x, ...) {
-        i <- x$getinverse()                                                     # the getinverse() function is used to check if the inverse of the matrix has already been computed and cached to x.
+        i <- x$getinverse()                                     # the getinverse() function is used to check if the inverse of the matrix has already been computed and cached to x.
         if(!is.null(i)) {
                 message("getting cached data")
                 return(i)
         } 
-        data <- x$get()                                                         # if the inverse of the matrix has not yet been cached, the get() function is used to retrieve the original matrix from x.
-        i <- solve(data, ...)                                                   # the solve() function computes the inverse of the original matrix.
-        x$setinverse(i)                                                         # the setinverse() function is used to cache the inverse of the matrix to x.
+        data <- x$get()                                         # if the inverse of the matrix has not yet been cached, the get() function is used to retrieve the original matrix from x.
+        i <- solve(data, ...)                                   # the solve() function computes the inverse of the original matrix.
+        x$setinverse(i)                                         # the setinverse() function is used to cache the inverse of the matrix to x.
         i   
 }
 
